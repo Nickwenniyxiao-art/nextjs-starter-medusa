7b846bd1aa7b92741206e8e5df37612555bcd96d
Nickwenniyxiao-art <Nick_wenniyxiao@icloud.com>
2026-03-07 19:04:17 -0800

    fix: complete order page i18n coverage (#45)

--- src/modules/order/components/help/index.tsx
+++ src/modules/order/components/help/index.tsx
@@ -1,20 +1,25 @@
+"use client"
+
 import { Heading } from "@medusajs/ui"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import { useTranslations } from "next-intl"
 import React from "react"
 
 const Help = () => {
+  const t = useTranslations("order")
+
   return (
     <div className="mt-6">
-      <Heading className="text-base-semi">Need help?</Heading>
+      <Heading className="text-base-semi">{t("needHelp")}</Heading>
       <div className="text-base-regular my-2">
         <ul className="gap-y-2 flex flex-col">
           <li>
-            <LocalizedClientLink href="/contact">Contact</LocalizedClientLink>
+            <LocalizedClientLink href="/contact">
+              {t("contactLink")}
+            </LocalizedClientLink>
           </li>
           <li>
-            <LocalizedClientLink href="/contact">
-              Returns & Exchanges
-            </LocalizedClientLink>
+            <LocalizedClientLink href="/contact">{t("returnsExchanges")}</LocalizedClientLink>
           </li>
         </ul>
       </div>
